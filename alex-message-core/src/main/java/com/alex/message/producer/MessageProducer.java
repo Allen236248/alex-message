d02042a320d14d27f13134b686a84f949636c869
wanglun <wanglun@yunnex.com>
2020-04-29 16:23:49 +0800

    整理逻辑

--- alex-message-core/src/main/java/com/alex/message/producer/MessageProducer.java
+++ alex-message-core/src/main/java/com/alex/message/producer/MessageProducer.java
@@ -1,9 +1,7 @@
 package com.alex.message.producer;
 
 /**
- * 消息服务接口(已废弃，请使用新版消息服务)
- * 一、引用新版yunnex-message服务，使用方法http://wiki.corp.yunnex.com/pages/viewpage.action?pageId=14978357
- * 二、如需将旧版本队列迁移到新版本消息服务中,请联系平台架构部增加路由信息
+ * 消息服务接口
  */
 public interface MessageProducer {
 
